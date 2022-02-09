72341a724d6dfdb46435b088faff630ce4bc9d07
Hakan <sanials4life2@gmail.com>
2022-02-09 23:38:35 +0100

    Hash Salt Password Bcrypt & Jwt added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,4 +10,9 @@ export class AuthController {
   signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
   }
+
+  @Post('/signin')
+  signIn(@Body(ValidationPipe) authCredentials: AuthCredentialsDto): Promise<{accessToken: string}> {
+    return this.authService.signIn(authCredentials);
+  }
 }
